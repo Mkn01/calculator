8a4fe658436b82dbe49a87d60cabd780ef3651eb
gurmanpreet nagra <gurmanpreetnagra@gmail.com>
2022-04-09 14:21:53 +0100

    initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+// es6 function or arrow function
+const calculator = (num1, num2, operator) => {
+  // all your function code goes here
+};
+
+const result = calculator(2, 3, "add");
+console.log(result);
